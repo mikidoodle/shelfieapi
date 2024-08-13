f65a9137a3f0cca6d33295879a8642105162d913
mikidoodle <mihir@pidgon.com>
2024-08-13 22:02:37 +0530

    Fixed login

--- pages/api/getSwipes.js
+++ pages/api/getSwipes.js
@@ -8,14 +8,6 @@ export default async function handler(req, res) {
    */
   const { uuid } = req.body;
   let getUserSwipes = await sql`SELECT swipes from users where uuid=${uuid}`;
-  let getUserLibrary = await sql`SELECT isbn from users where uuid=${uuid}`;
-  let library =
-    getUserLibrary[0].isbn.length > 15
-      ? getUserLibrary[0].isbn.slice(0, 15)
-      : getUserLibrary[0].isbn;
-  for (var i = 0; i < library.length; i++) {
-    library[i] = JSON.parse(decodeURIComponent(library[i])).title;
-  }
   let swipes =
     getUserSwipes[0].swipes.length > 10
       ? getUserSwipes[0].swipes.slice(0, 10)
@@ -24,10 +16,7 @@ export default async function handler(req, res) {
   //JSON stringiyf the swipes array
   let prompt = `Get 10 bestselling/trending book recommendations based on user's past 10 books: ${JSON.stringify(
     swipes.reverse()
-  )}. 15 library books: ${JSON.stringify(
-    library.reverse()
-  )}. If empty return 15 all time top books. Return only book titles in a JS array. Recommend equally from liked genres. No obscure books. Bestsellers/trending only. Library books have lesser weight than past 10 books. No additional text.
-`;
+  )}. If empty return 15 all time top books. Return only book titles in a JS string array. Recommend equally from liked genres. No obscure books or books already in the list; THIS IS IMPORTANT. Bestsellers/trending only. No additional text.`;
   console.log(prompt);
   var getSuggestions = await fetch(
     "https://jamsapi.hackclub.dev/openai/chat/completions",
@@ -52,7 +41,7 @@ export default async function handler(req, res) {
 
   var response = await getSuggestions.json();
   return res.status(200).json({ message: response.choices[0].message.content });
- /* return res
+  /* return res
     .status(200)
     .json({
       message: JSON.stringify([
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,39 +1,46 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import postgres from "postgres";
 const sql = postgres(process.env.PG_CONNECT);
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 export default async function handler(req, res) {
   /**
    * @param {string} username
    * @param {string} password
    */
-  
+
   const { username, password } = req.body;
-  console.log(`login ${username.includes("@") && username.includes(".") ? 'email' : 'username'}`)
+  console.log(
+    `login ${
+      username.includes("@") && username.includes(".") ? "email" : "username"
+    }`
+  );
   if (!username || !password) {
-    return res.status(400).json({ message: "Missing username or password", error: true });
+    return res
+      .status(400)
+      .json({ message: "Missing username or password", error: true });
   }
   let user = [];
-  if(username.includes("@") && username.includes(".")) {
-   user = await sql`SELECT password, uuid, username FROM users WHERE email = ${username}`;
+  if (username.includes("@") && username.includes(".")) {
+    user =
+      await sql`SELECT password, uuid, username FROM users WHERE email = ${username}`;
   } else {
-    user = await sql`SELECT password, uuid, username FROM users WHERE username = ${username}`;
+    user =
+      await sql`SELECT password, uuid, username FROM users WHERE username = ${username}`;
   }
-  console.log(user)
-  if(user.length === 0){
-    return res.status(400).json({message: "User not found", error: true})
+  console.log(user);
+  if (user.length === 0) {
+    return res.status(400).json({ message: "User not found", error: true });
   } else {
-    const match = bcrypt.compare(password, user[0].password, function(err, result){
-      if(result){
-        return res.status(200).json({
-          message: "Login successful",
-          uuid: user[0].uuid,
-          username: user[0].username,
-          error: false
-        })
-      } else {
-        return res.status(400).json({message: "Invalid password", error: true})
-      }
-    })
+    var match = await bcrypt.compare(password, user[0].password);
+    if (match) {
+      return res.status(200).json({
+        message: "Login successful",
+        uuid: user[0].uuid,
+        username: user[0].username,
+        error: false,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid password", error: true });
+    }
   }
 }
